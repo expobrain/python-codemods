0b684a9d7e0fed7274c1501b7e6cd34535e678c4
Daniele Esposti <daniele.esposti@gmail.com>
2020-07-05 23:52:48 +0100

    Renamed wxPython constants

--- codemods/wx_python.py
+++ codemods/wx_python.py
@@ -22,3 +22,31 @@ class ColorToColourCommand(VisitorBasedCodemodCommand):
             return updated_node.with_changes(attr=cst.Name(value="Colour"))
 
         return updated_node
+
+
+class ConstantsRenameCommand(VisitorBasedCodemodCommand):
+
+    DESCRIPTION: str = "Rename constants"
+
+    matchers_map = {
+        matchers.Attribute(
+            value=matchers.Name(value="wx"), attr=matchers.Name(value=name)
+        ): renamed
+        for name, renamed in [
+            ("WXK_PRIOR", "WXK_PAGEUP"),
+            ("WXK_NEXT", "WXK_PAGEDOWN"),
+            ("WXK_NUMPAD_PRIOR", "WXK_NUMPAD_PAGEUP"),
+            ("WXK_NUMPAD_NEXT", "WXK_NUMPAD_PAGEDOWN"),
+            ("OPEN", "FD_OPEN"),
+            ("FILE_MUST_EXIST", "FD_FILE_MUST_EXIST"),
+        ]
+    }
+
+    def leave_Attribute(
+        self, original_node: cst.Attribute, updated_node: cst.Attribute
+    ) -> cst.Attribute:
+        for matcher, renamed in self.matchers_map.items():
+            if matchers.matches(updated_node, matcher):
+                return updated_node.with_changes(attr=cst.Name(value=renamed))
+
+        return updated_node
--- tests/wx_python_test.py
+++ tests/wx_python_test.py
@@ -1,6 +1,6 @@
 from libcst.codemod import CodemodTest
 
-from codemods.wx_python import ColorToColourCommand
+from codemods.wx_python import ColorToColourCommand, ConstantsRenameCommand
 
 
 class ColorToColourCommandTests(CodemodTest):
@@ -16,5 +16,46 @@ class ColorToColourCommandTests(CodemodTest):
     def test_reference_substitution(self) -> None:
         before = "isinstance(a, wx.Color)"
         after = "isinstance(a, wx.Colour)"
+
+        self.assertCodemod(before, after)
+
+
+class ConstantsRenameCommandTests(CodemodTest):
+
+    TRANSFORM = ConstantsRenameCommand
+
+    def test_WXK_PRIOR_substitution(self) -> None:
+        before = "wx.WXK_PRIOR"
+        after = "wx.WXK_PAGEUP"
+
+        self.assertCodemod(before, after)
+
+    def test_WXK_NEXT_substitution(self) -> None:
+        before = "wx.WXK_NEXT"
+        after = "wx.WXK_PAGEDOWN"
+
+        self.assertCodemod(before, after)
+
+    def test_WXK_NUMPAD_PRIOR_substitution(self) -> None:
+        before = "wx.WXK_NUMPAD_PRIOR"
+        after = "wx.WXK_NUMPAD_PAGEUP"
+
+        self.assertCodemod(before, after)
+
+    def test_WXK_NUMPAD_NEXT_substitution(self) -> None:
+        before = "wx.WXK_NUMPAD_NEXT"
+        after = "wx.WXK_NUMPAD_PAGEDOWN"
+
+        self.assertCodemod(before, after)
+
+    def test_OPEN_substitution(self) -> None:
+        before = "wx.OPEN"
+        after = "wx.FD_OPEN"
+
+        self.assertCodemod(before, after)
+
+    def test_FILE_MUST_EXIST_substitution(self) -> None:
+        before = "wx.FILE_MUST_EXIST"
+        after = "wx.FD_FILE_MUST_EXIST"
 
         self.assertCodemod(before, after)
